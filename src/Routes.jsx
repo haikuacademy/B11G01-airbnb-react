fc820010a4cc94fbb55c8a643124374c6d831349
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-27 12:20:14 +0700

    Deleted home route and updated house route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from './components/Home'
 import Signup from './components/Signup'
 import Listings from './components/Listings'
 import Profile from './components/Profile'
@@ -14,12 +13,11 @@ function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
                 <Route path='/Signup' element={<Signup />} />
                 <Route path='/Listings' element={<Listings />} />
                 <Route path='/Profile' element={<Profile />} />
                 <Route path='/Bookings' element={<Bookings />} />
-                <Route path='/houses' element={<Houses />} />
+                <Route path='/' element={<Houses />} />
                 <Route path='/houses/:id' element={<House />} />
                 <Route path='/houses/:id/edit' element={<HouseEdit />} />
                 <Route path='/login' element={<Login />} />
