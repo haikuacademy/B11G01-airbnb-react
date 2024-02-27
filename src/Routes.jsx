e43ae27ac70cd98a30b11dde1230f4e27df8d047
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-27 12:05:32 +0700

    Added routes for Houses, House, HouseEdit, and Login

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from './components/Home'
+import Houses from './components/Houses'
+import House from './components/House'
+import HouseEdit from './components/HouseEdit'
+import Login from './components/Login'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Houses />} />
+                <Route path='/houses/:id' element={<House />} />
+                <Route path='/houses/:id/edit' element={<HouseEdit />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
