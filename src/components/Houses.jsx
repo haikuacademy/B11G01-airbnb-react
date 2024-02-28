805046a9e33861017e7522aa580872edc9030f8a
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-28 10:50:33 +0700

    fixed filters

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -4,15 +4,8 @@ import Filters from './Filters'
 function Houses() {
   return (
     <>
-      {/* filters */}
-      <div className="flex justify-between gap-1 bg-blue-200">
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-      </div>
+      {/* search bar */}
+      <Filters />
       {/* gallery */}
       <div className="grid gap-2 grid-cols-5 bg-pink-200">
         <HouseCard />
