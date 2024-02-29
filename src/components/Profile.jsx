fb7f3862d322210fe4c8c05274eb89b42a1d54cb
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-29 13:57:43 +0700

    Added Profile form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,32 @@ function Profile() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="gap-1 flex justify-start bg-blue-200">
-        <div className="flex bg-blue-600">1</div>
-        <div className="flex bg-blue-600">2</div>
+      <div className="grid grid-cols-2 gap-3 border w-full p-3 mt-2 rounded">
+        <form>
+          <div className="text-lg font-bold">Your Profile</div>
+          <div className="flex cols-2 p-3 gap-3">
+            <img
+              src="https://randomuser.me/api/portraits/women/69.jpg"
+              alt="User"
+              className="w-12 rounded-full"
+            ></img>
+            <input type="text" className="border w-full p-2 rounded" />
+          </div>
+          <div className="text-sm text-gray-400 pt-2">First Name</div>
+          <input type="text" className="border w-full p-2 rounded" />
+          <div className="text-sm text-gray-400 pt-2">Last Name</div>
+          <input type="text" className="border w-full p-2 rounded" />
+          <div className="text-sm text-gray-400 pt-2">Email</div>
+          <input type="text" className="border w-full p-2 rounded" />
+          <div className="flex justify-between gap-2">
+            <button className="flex border rounded px-2 pt-2 pb-2 text-sm bg-red-400 text-white mt-4">
+              Save Changes
+            </button>
+            <button className="flex border rounded px-2 pt-2 pb-2 text-sm mt-4">
+              Logout
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
