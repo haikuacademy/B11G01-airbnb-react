ef53239f49dd347e64aba6b6ae0f1487610225a7
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-29 13:28:45 +0700

    Added Edit House form

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -4,9 +4,91 @@ function HouseEdit() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="grid grid-cols-2 gap-3">
-        <div className="">1</div>
-        <div className="">2</div>
+      <div className="grid grid-cols-2 gap-3 border w-full p-3 mt-2 rounded">
+        <form>
+          {/* Location */}
+          <div>Edit your Listing</div>
+          <div className="text-sm text-gray-300 pt-2">Location</div>
+          <input
+            type="text"
+            placeholder="Bali"
+            className="border w-full p-2 rounded"
+          />
+          {/* Bedrooms */}
+          <div className="text-sm text-gray-300 pt-2">Bedrooms</div>
+          <input
+            type="number"
+            placeholder="3"
+            className="border w-full p-2 rounded"
+          />
+          {/* Bathrooms */}
+          <div className="text-sm text-gray-300 pt-2">Bathrooms</div>
+          <input
+            type="number"
+            placeholder="2"
+            className="border w-full p-2 rounded"
+          />
+          {/* Price per Night */}
+          <div className="text-sm text-gray-300 pt-2">Price per Night</div>
+          <input
+            type="number"
+            placeholder="140"
+            className="border w-full p-2 rounded"
+          />
+          {/* Description */}
+          <div className="text-sm text-gray-300 pt-2">Description</div>
+          <textarea
+            className="border w-full p-2 rounded"
+            placeholder="This enchanting Airbnb villa in Bali..."
+          ></textarea>
+          <div className="flex justify-left gap-2">
+            <button className="flex border rounded px-2 pt-2 pb-2 text-sm bg-red-400 text-white mt-4">
+              Save Changes
+            </button>
+            <button className="flex border rounded px-2 pt-2 pb-2 text-sm mt-4">
+              Cancel
+            </button>
+          </div>
+        </form>
+        <div className="">
+          {/* Photos */}
+          <div className="text-sm text-gray-300">Photos</div>
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+          <input
+            type="text"
+            placeholder="ImageURL.png"
+            className="border w-full p-2 rounded mt-2"
+          />
+        </div>
       </div>
     </div>
   )
