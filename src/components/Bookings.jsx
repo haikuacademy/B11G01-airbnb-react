ac650a5eaaf850cf2e8368ec9503af6997a824a6
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-03-01 12:08:41 +0700

    Update Bookings.jsx

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,23 +1,18 @@
 import Nav from './Nav'
+import HouseCard from './HouseCard'
 
 function Bookings() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="grid grid-cols-5  gap-3">
-        <div className="">1</div>
-        <div className="">2</div>
-        <div className="">3</div>
-        <div className="">4</div>
-        <div className="">5</div>
-        <div className="">6</div>
-        <div className="">7</div>
-        <div className="">8</div>
-        <div className="">9</div>
-        <div className="">10</div>
+      {/* listings gallery */}
+      <div className="grid gap-2 grid-cols-5">
+        <HouseCard />
+        <HouseCard />
       </div>
     </div>
   )
 }
+
 
 export default Bookings
