8d9689ecf226b7170b7df8e50de22fde8f56ee57
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-03-02 13:57:12 +0700

    updated reviews with data from array

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,46 +1,53 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCommentDots, faStar } from '@fortawesome/free-solid-svg-icons'
 
-const reviews = [
-  {
-    content: 'This product exceeded my expectations! I love it.',
-    rating: 5,
-    date: '2024-03-02',
-    author: {
-      firstName: 'John',
-      lastName: 'Jones',
-      picture: 'https://example.com/john_jones_picture.jpg'
-    }
-  },
-  {
-    content: 'Not satisfied with the quality. It could be better.',
-    rating: 2,
-    date: '2024-03-01',
-    author: {
-      firstName: 'Alice',
-      lastName: 'Smith',
-      picture: 'https://example.com/alice_smith_picture.jpg'
-    }
-  },
-  {
-    content: 'Great service and fast delivery!',
-    rating: 4.5,
-    date: '2024-02-28',
-    author: {
-      firstName: 'Jim',
-      lastName: 'Crawford',
-      picture: 'https://example.com/jim_crawford_picture.jpg'
-    }
-  }
-]
-
 export default function Reviews() {
+  const reviews = [
+    {
+      content:
+        'This place exceeded my expectations! I would definitely return.',
+      rating: 5,
+      date: '2024-03-02',
+      author: {
+        firstName: 'Ryan',
+        lastName: 'Jones',
+        picture: 'https://randomuser.me/api/portraits/men/71.jpg'
+      }
+    },
+    {
+      content: 'Not satisfied with the house. It could be better.',
+      rating: 2,
+      date: '2024-03-01',
+      author: {
+        firstName: 'Karen',
+        lastName: 'Trash',
+        picture: 'https://randomuser.me/api/portraits/women/53.jpg'
+      }
+    },
+    {
+      content: 'Great location and super responsive host!',
+      rating: 4.5,
+      date: '2024-02-28',
+      author: {
+        firstName: 'Jim',
+        lastName: 'Crawford',
+        picture: 'https://randomuser.me/api/portraits/men/11.jpg'
+      }
+    }
+  ]
+
+  const averageRating =
+    reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length
+
   return (
     <div className="container mx-auto gap-2">
       <div className="flex gap-2 items-center">
         <div className="inline mb-4">
           <FontAwesomeIcon icon={faCommentDots} />
-          <span className="text-lg font-bold ml-1"> 34 Reviews</span>
+          <span className="text-lg font-bold ml-1">
+            {' '}
+            {reviews.length} Reviews
+          </span>
           <div>
             <FontAwesomeIcon
               icon={faStar}
@@ -50,32 +57,36 @@ export default function Reviews() {
               className="text-sm ml-6
             "
             >
-              Average Rating: 2.3{' '}
+              Average Rating: {averageRating.toFixed(2)}
             </span>
           </div>
         </div>
       </div>
       <div className="space-y-3">
-        <Review />
-        <Review />
+        {reviews.map((review, index) => (
+          <Review key={index} review={review} />
+        ))}
       </div>
     </div>
   )
 }
 
-function Review() {
+function Review(props) {
+  const review = props.review
   return (
     <div className="border border-solid border-slate-300 rounded-lg p-3 m-2 text-sm bg-white">
       <div className="flex cols-2 p-3 border-slate-300 gap-3">
         <img
-          src="https://randomuser.me/api/portraits/women/53.jpg"
+          src={review.author.picture}
           alt="User"
           className="w-10 rounded-full"
         ></img>
         <div className="inline text-slate-400">
-          <div>12.12.2023 </div>
+          <div>{review.date}</div>
           <div className="text-black">
-            <div>Karen Trash</div>
+            <div>
+              {review.author.firstName} {review.author.lastName}
+            </div>
           </div>
         </div>
       </div>
@@ -89,13 +100,11 @@ function Review() {
           icon={faStar}
           className="absolute mt-1 ml-5 pointer-events-none text-yellow-500"
         />
-        <div className="pl-11">2</div>
+        <div className="pl-11">{review.rating}</div>
       </div>
       <div className="grid justify-items-start p-3 items-center">
         <p>
-          Awful place, the pool is cold, the beer is warm. My husband still
-          drunk all the time, and it was also raining a lot. I suggest the
-          owners to sort these out ASAP. Otherwise I wont return, ever.
+          <ul></ul>
         </p>
       </div>
     </div>
