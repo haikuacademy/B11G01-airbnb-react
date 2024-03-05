12c208b8e685eeec6deda77f33eb7e81af94df8e
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-03-05 19:35:08 +0700

    replace the hardcoded values

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -37,35 +37,35 @@ function HouseEdit() {
           <div className="text-sm text-gray-300 pt-2">Location</div>
           <input
             type="text"
-            placeholder="Bali"
+            placeholder={house.location}
             className="border w-full p-2 rounded"
           />
           {/* Bedrooms */}
           <div className="text-sm text-gray-300 pt-2">Bedrooms</div>
           <input
             type="number"
-            placeholder="3"
+            placeholder={house.rooms}
             className="border w-full p-2 rounded"
           />
           {/* Bathrooms */}
           <div className="text-sm text-gray-300 pt-2">Bathrooms</div>
           <input
             type="number"
-            placeholder="2"
+            placeholder={house.bathrooms}
             className="border w-full p-2 rounded"
           />
           {/* Price per Night */}
           <div className="text-sm text-gray-300 pt-2">Price per Night</div>
           <input
             type="number"
-            placeholder="140"
+            placeholder={house.price}
             className="border w-full p-2 rounded"
           />
           {/* Description */}
           <div className="text-sm text-gray-300 pt-2">Description</div>
           <textarea
             className="border w-full p-2 rounded"
-            placeholder="This enchanting Airbnb villa in Bali..."
+            placeholder={house.description}
           ></textarea>
           <div className="flex justify-left gap-2">
             <button className="flex border rounded px-2 pt-2 pb-2 text-sm bg-red-400 text-white mt-4">
